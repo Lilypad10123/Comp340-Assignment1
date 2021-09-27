0d4193a8843dc94b703d5a12b73fe78b7eac0dea
Lilypad10123 <julia.wosnack@student.ufv.ca>
2021-09-27 13:39:23 -0700

    Assignment specifications

--- QuicksortProject.c
+++ QuicksortProject.c
@@ -1,8 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//struct
+
+quick_sort(int lo, int hi) {
+	//return from smallest case of one element subproblem
+
+	//choose the first element of the subproblem array section
+
+	int left = lo; //indices [lo...left-1] elements less than pivot
+	int right = hi; //indices [right+1...hi] greater than pivot
+	int i = lo + 1; //indices [left... i-1] equal to pivot element
+	// indices [i...right] elements not yet compared against pivot
+
+	//loop until index i increments past index right
+	//{
+		//case 1: i less than pivot, swap i and left
+			//increment i and left
+		//case 2: i greater than pivot, swap i and right
+			//decrement right
+		//case 3: i equal to pivot
+			//increment i
+	//}
+	//recurse and sort elements smaller than pivot
+	//recurse and sort elements larger than pivot
+
+}
+
+
+
+
 int main() {
 
-
+	//run quicksort()
 	return 0;
 }
