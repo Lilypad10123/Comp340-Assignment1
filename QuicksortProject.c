ed2d78bf98750dfa29464c524d911f8623766fe2
Lilypad10123 <julia.wosnack@student.ufv.ca>
2021-09-28 15:42:17 -0700

    working on quicksort function, weird error

--- QuicksortProject.c
+++ QuicksortProject.c
@@ -11,9 +11,12 @@ void printArray(IntArray *array) {
 		printf("%i ", (*array).thisArray);
 		array++;
 	}
+	for(i=0; i<5; i++) {
+		array--;
+	}
 	printf("\n");
 }
-void swap_elements(IntArray *array, int element1, int element2) {
+void  swap_elements(IntArray *array, int element1, int element2) {
 	int temp[5];
 	int i;
 	for(i=0; i<5; i++) {
@@ -25,10 +28,10 @@ void swap_elements(IntArray *array, int element1, int element2) {
 	temp[element1] = temp[element2];
 	temp[element2] = t;
 
-	for(i=0; i<5; i++) {
-		printf("%i ", temp[i]);
+	for(i=5, i>0; i--) {
+		(*array).thisArray = temp[i];
+		array--;
 	}
-	printf("\n");
 }
 
 void quick_sort(IntArray *array, int lo, int hi) {
@@ -38,41 +41,39 @@ void quick_sort(IntArray *array, int lo, int hi) {
 		temp[i] = (*array).thisArray;
 		array++;
 	}
-	int pivot_element = temp[0];
-	//printf("%i\n", pivot_element);
+	int pivot_element = temp[sizeof(temp)/sizeof(int)];
 
 	int left = lo; //indices [lo...left-1] elements less than pivot
 	int right = hi; //indices [right+1...hi] greater than pivot
 	int j = lo + 1; //indices [left... i-1] equal to pivot element
-	// indices [i...right] elements not yet compared against pivot
+	int k = hi - j;
 
-	//loop until index i increments past index right
-	//{
-		//case 1: i less than pivot, swap i and left
-			//increment i and left
-		//case 2: i greater than pivot, swap i and right
-			//decrement right
-		//case 3: i equal to pivot
-			//increment i
-	//}
+	while (j<right) {
+		if(j<pivot_element) {
+			swap_elements(array, j, left)
+			j++;
+			left++;
+		} else if (j>pivot_element) {
+			swap_elements(array, j, right);
+			right--;
+		} else if (j==pivot_element) {
+			j++;
+		}
+
+	}
 	//recurse and sort elements smaller than pivot
 	//recurse and sort elements larger than pivot
-
 }
-
-
 
 
 int main() {
 	IntArray intArray[] = {30, 20, 50, 10, 40};
 
+	int lo = 0;
+	int hi = 4;
 	printArray(intArray);
-
-
-	//run quicksort()
-
-	swap_elements(intArray,0,1);
-	//quick_sort(intArray,1,1);
+	quick_sort(intArray, lo, hi);
+	printArray(intArray);
 
 	return (EXIT_SUCCESS);
 }
